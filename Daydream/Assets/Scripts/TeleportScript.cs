56d50cf711f55af0ee13c99fe8ee69cd528d5a7f
Tuuli Sarantola <tuuli.sarantola@gmail.com>
2017-10-18 22:15:13 +0300

    Stub script for TeleportScript

---
+++ Daydream/Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour {
+
+	public GameObject target;
+	public GameObject Headset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GvrControllerInput.ClickButtonDown) {
+			// If the Pointer currently has a non-null target
+			if (PointerDot.target != null) {
+				// Move the Headset at a fixed height above Pointer’s hit point.
+				Headset.transform.position = PointerDot.target.position; // plus fixed height ":D"
+			}
+		}
+	}
+}
