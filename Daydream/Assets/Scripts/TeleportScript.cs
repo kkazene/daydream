1ab1075f35b4066e7a79f9019e377d120e21317d
Klaus Helenius <klaus.helenius@gmail.com>
2017-10-24 02:37:51 +0300

    Removed unneeded privates from teleport

--- Daydream/Assets/Scripts/TeleportScript.cs
+++ Daydream/Assets/Scripts/TeleportScript.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class TeleportScript : MonoBehaviour {
 
-	GameObject headset;
-
 	void Start () {
-        headset = GameObject.Find("Headset");
 	}
 
 	void Update () {
 		if (GvrControllerInput.ClickButtonDown) {
 			var pointer = GameObject.Find("Controller").GetComponent<Pointer>();
 			if (pointer.target != null) {
-				headset.transform.position = pointer.hitLocation + Vector3.up * 1f;
+				transform.position = pointer.hitLocation + Vector3.up * 1f;
 			}
 		}
 	}
